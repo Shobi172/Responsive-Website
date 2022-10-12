2ac5d8338cecfc9e30ff55e1781d0c5e13216b3e
Shobi172 <shobinshaju2001@gmail.com>
2022-10-12 16:26:39 +0530

    Website Completed

---
+++ E-T-Q-responsive-site--main/script.js
@@ -0,0 +1,8 @@
+var nav=document.querySelector('nav');
+window.addEventListener('scroll',function(){
+    if(window.pageYOffset > 100){
+        nav.classList.add('bg-light','shadow');
+    }else{
+        nav.classList.remove('bg-light','shadow');
+    }
+})
